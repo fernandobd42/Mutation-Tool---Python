669d866602d8aa219fda72cf6430a3f20896f2c9
fernandobd43 <fernandolucasbd@gmail.com>
2016-10-10 15:37:33 -0400

    Mutation v.04

--- Mutation01.py
+++ Mutation01.py
@@ -4,38 +4,44 @@ import shutil
 from os import makedirs
 
 class Mutation():
+    originalJavaFile = ""
 
     def createNewDir(self):
-        if (os.path.exists('Arquivos')):
+        if (os.path.exists('Files')):
             current_directory = os.path.dirname(os.path.abspath(__file__))
-            shutil.rmtree(current_directory + '/Arquivos/')
-            makedirs("Arquivos")
+            shutil.rmtree(current_directory + '/Files/')
+            makedirs("Files")
         else:
-            makedirs("Arquivos")
+            makedirs("Files")
 
     def mutate(self):
         i = 0
         numFile = 0
         data = []
-        file = ('./aux/arquivo.java')
 
-        fileJava = open(file, 'r')
-        for line in fileJava:
-            i+=1
-            if(line.find('Hello world') > -1):
-                data.append(i)
-
-        for mutationPosition in data:
-            numFile += 1
-            print('arquivo',numFile)
-            i = 0
+        if (self.originalJavaFile != ""):
+            file = self.originalJavaFile
             fileJava = open(file, 'r')
-            mutantFile = open('./Arquivos/arquivo'+str(numFile)+'.java', 'w')
+
             for line in fileJava:
-                i += 1
-                if (mutationPosition == i):
-                    mutantFile.write(line.replace('Hello world', 'Hello Moon', 1))
-                else:
-                    mutantFile.write(line)
-            mutantFile.close()
-        fileJava.close()
+                i+=1
+                if(line.find('Hello world') > -1):
+                    data.append(i)
+
+            for mutationPosition in data:
+                numFile += 1
+                print('Mutant',numFile)
+                i = 0
+                fileJava = open(file, 'r')
+                mutantFile = open('./Files/Mutant'+str(numFile)+'.java', 'w')
+                for line in fileJava:
+                    i += 1
+                    if (mutationPosition == i):
+                        mutantFile.write(line.replace('Hello world', 'Hello Moon', 1))
+                    else:
+                        mutantFile.write(line)
+                mutantFile.close()
+            fileJava.close()
+            print('Success of mutation')
+        else:
+            print('File doesn\'t exist')
