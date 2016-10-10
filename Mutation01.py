8d1c0d91c84c046ccfcefa8fbf8e6c7af783b3cb
fernandobd43 <fernandolucasbd@gmail.com>
2016-10-10 19:09:04 -0400

    Mutation v.05

--- Mutation01.py
+++ Mutation01.py
@@ -1,11 +1,12 @@
-import os
-import glob
-import shutil
-from os import makedirs
+import os # the 'os' module provides functions to interact with the operating system
+import shutil # the 'shutil' module provides a higher level interface for file management tasks and  directories that is too easier to use.
+from os import makedirs # the 'makedirs' is a recursive directory creation function
 
+# the class Mutation is used to make a mutation on the files .java in the moment, in future it will be used to make mutation in programs
 class Mutation():
-    originalJavaFile = ""
+    originalJavaFile = "" # variable used to get the path of file .java
 
+    # method used to create a new directory where the mutated files will be saved
     def createNewDir(self):
         if (os.path.exists('Files')):
             current_directory = os.path.dirname(os.path.abspath(__file__))
@@ -14,20 +15,24 @@ class Mutation():
         else:
             makedirs("Files")
 
+    # method used to do the mutations on the file .java
     def mutate(self):
         i = 0
         numFile = 0
         data = []
 
+        # selection structure used to validate the variable 'originalJavaFile' got the path of the file .java
         if (self.originalJavaFile != ""):
             file = self.originalJavaFile
             fileJava = open(file, 'r')
 
+            # repetition structure used to get the lines who'll be mutated, and inserting them in the array
             for line in fileJava:
                 i+=1
                 if(line.find('Hello world') > -1):
                     data.append(i)
 
+            # repetition structure used to create the mutated files where needed in accordance with the array 'data', which has the positions to be mutated
             for mutationPosition in data:
                 numFile += 1
                 print('Mutant',numFile)
@@ -44,4 +49,5 @@ class Mutation():
             fileJava.close()
             print('Success of mutation')
         else:
+            # if the file isn't selected, the program return this
             print('File doesn\'t exist')
