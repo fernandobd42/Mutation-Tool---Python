1b15128a1b670ac8bfbb85c30a0cb0c1fd6cb21a
fernandobd43 <fernandolucasbd@gmail.com>
2017-03-16 22:30:25 -0400

    mutate used to do mutation in mutant programs

--- mutate.py
+++ mutate.py
@@ -1,18 +1,22 @@
 import os # the 'os' module provides functions to interact with the operating system
-import sys # The sys module provides a number of functions and variables that can be used to manipulate different parts of the Python.
+import sys # The 'sys' module provides a number of functions and variables that can be used to manipulate different parts of the Python.
+
 from data import Data # Import the class Data of file data.py
 from getOperatorsJson import GetOperator # Import the class GetOperator of file getOperatorsJson.py
 from operatorLines import OperatorLines # Import the class OperatorLines of file operatorLines.py
 from handlingDirectories import HandlingDirectories # Import the class HandlingDirectories of file handlingDirectories.py
 from log import * # import all of the file log.py
+from screenshot import Screenshot # import the class Screenshot of file screenshot.py
 
 # the class Mutate is used to do the mutation on the mutated programs
 class Mutate():
-
     # instantiating the method log of the class Log
     Log().log(os.getcwd())
+
     # method used to do the mutation
     def mutate(self):
+        pathOrigin = os.getcwd()
+        dstImages = pathOrigin + '/Images/'
         count = 0
         src = unicode(Data.pathProject)
         dst = ""
@@ -21,6 +25,7 @@ class Mutate():
         #repetition structure used to read each one operator of the json
         for op in operators['Operators']:
             num = 0
+            name = op['name'].encode('utf-8')
             op1 = op['op1'].encode('utf-8')
             op2 = op['op2'].encode('utf-8')
             ext = op['ext'].encode('utf-8')
@@ -34,19 +39,22 @@ class Mutate():
                 for mutateLine in lines:
                     num += 1
                     count += 1
-                    dst = os.getcwd()+'/Mutants/'+str(count)+'-Mutant-'+str(op1)+'-'+str(num)
-                    cloneDir = HandlingDirectories().cloneDir(src, dst)
-                    mutation = Mutate().replace(dst, mutateLine, op1, op2, ext, count)
+                    nameImage = 'imagem'+str(count)
+                    pathMutant = pathOrigin +'/Mutants/'+str(count)+'-Mutant-'+name
+                    HandlingDirectories().cloneDir(src, pathMutant)
+                    Mutate().replace(pathMutant, ext, mutateLine, count, op1, op2)
+                    Screenshot().getScreenshot(pathMutant, dstImages, nameImage)
+
             else:
                 logging.error("No one of the operators past by the json was found in the original program")
                 print("No one of the operators past by the json was found in the original program")
 
 
     # method used to replace operators
-    def replace(self, path, mutateLine, op1, op2, ext, count):
+    def replace(self, pathMutant, ext, mutateLine, count, op1, op2):
         i = 0
         #repetition structure used to swepper the directory 'path'
-        for root, dirs, files in os.walk(path):
+        for root, dirs, files in os.walk(pathMutant):
             #repetition structure used to get only 'file' on this directory
             for file in files:
                 #selection structure used to get only files with determined extension 'ext'
