7eb8edc1f0a67a9ea977394bfa8b74169dcba38c
fernandobd43 <fernandolucasbd@gmail.com>
2017-03-16 22:08:33 -0400

    data implements func getMain

--- data.py
+++ data.py
@@ -1,5 +1,6 @@
 # the class Data is used define the variable pathProject
 class Data():
+    
     # Method to set and get the path of the program to be tested
     def pathProject(self):
         return self.pathProject
@@ -7,3 +8,7 @@ class Data():
     # Method to set and get the path of json file where has the operators
     def pathJson(self):
         return self.pathJson
+
+    # Method to set and get the path of Main of Java file where has the operators
+    def pathMain(self):
+        return self.pathMain
