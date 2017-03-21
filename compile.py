b0c2cc5a4d42582cc6abc1649237ddad95b9abe0
fernandobd43 <fernandolucasbd@gmail.com>
2017-03-20 21:09:26 -0400

    take screenshot only of gui java program

--- compile.py
+++ compile.py
@@ -3,6 +3,7 @@ import os # the 'os' module provides functions to interact with the operating sy
 import sys # the 'sys' module provides a number of functions and variables that can be used to manipulate different parts of the Python.
 import signal # the 'signal' module provides mechanisms to use signal handlers in Python
 import os.path # the module implements some useful functions on pathnames
+from winlaunch import *
 
 # the class Compile is used to compile and execute java program's
 class Compile():
@@ -14,8 +15,7 @@ class Compile():
         cmdJavac = 'javac ' + fileMain
         javac = subprocess.call(cmdJavac, shell=True)
 
-        cmdJava = 'java ' + os.path.splitext(fileMain)[0]
-        java = subprocess.Popen("exec " + cmdJava, signal.SIGTERM, shell=True)
+        wid, pid = launch('java ' + os.path.splitext(fileMain)[0])
 
-        pid = java.pid
-        return pid
+        win = win_size(wid)
+        return pid, win
--- screenshot.py
+++ screenshot.py
@@ -10,11 +10,11 @@ from compile import Compile # import the class Compile of file compile.py
 class Screenshot():
 
     # method used to take and save screenshots
-    def screenshot(self, dstImage, nameImage):
+    def screenshot(self, dstImage, nameImage, a,b):
         pathImage = dstImage + nameImage
 
         # grab screenshot and save image file
-        pyscreenshot.grab(backend='scrot').save(pathImage + '.png')
+        pyscreenshot.grab(bbox=(0,25,a,b), backend='scrot', childprocess=False).save(pathImage + '.png')
 
 
     # method used to compile and execute java program's, then take and save thescreenshot after kill the program
@@ -25,6 +25,10 @@ class Screenshot():
         java = Compile().compile_execute_java(pathMainFile, fileMain)
 
         time.sleep(1)
-        Screenshot().screenshot(dstImage, nameImage)
+        
+        a = java[1][0]
+        b = java[1][1] + 45
+        Screenshot().screenshot(dstImage, nameImage, a, b)
 
-        os.kill(java, signal.SIGKILL)
+
+        os.kill(int(java[0]), signal.SIGKILL)
