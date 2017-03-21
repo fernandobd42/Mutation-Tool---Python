8ac71835191158f3a1aee874a6b35938d4d011aa
fernandobd43 <fernandolucasbd@gmail.com>
2017-03-21 10:12:08 -0400

    compare images and return results

---
+++ compare.py
@@ -0,0 +1,27 @@
+import Image
+import ImageChops
+import math, operator
+import os.path
+
+class Compare():
+    count = []
+    mutants = []
+
+    def compare(self, image):
+
+        im1 = Image.open("/home/fernando/Documentos/GIT/Mutation-Tool---Python/Images/imagem1.png")
+        im2 = Image.open(image)
+
+        result = ImageChops.difference(im1, im2).getbbox() is None
+
+        if result is True:
+            self.count.append(1)
+            self.mutants.append(os.path.basename(image))
+        else:
+            pass
+
+        print "Quantidade de Mutantes Vivos sem alteracao na GUI: " + str(sum(self.count)-1)
+        print "Mutantes: "
+        print self.mutants[1:]
+
+        return "Quantidade de Mutantes Vivos sem alteracao na GUI: " + str(sum(self.count)-1) + ".\nEqual Mutants " + str(self.mutants[1:])
