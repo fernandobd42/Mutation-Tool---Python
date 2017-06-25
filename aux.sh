ec9db61ac95f27f02aa566952a2dccc88feb6e7e
Fernando <fernandolucasbd@gmail.com>
2017-06-25 01:09:53 -0300

    adjust aux.sh

--- aux.sh
+++ aux.sh
@@ -1,15 +1,15 @@
+apt-get install -y python-pip python-pil
 add-apt-repository ppa:openjdk-r/ppa
-apt-get install -y python-pip && python-pil
 apt-get update
-apt-get install -y openjdk-8-jdk && pyqt4-dev-tools && xdotool && scrot
+apt-get install -y openjdk-8-jdk pyqt4-dev-tools xdotool scrot 
 pip install -y pyscreenshot
 update-alternatives --config java
-apt-get upgrade -y
+sudo apt-add-repository --remove ppa:openjdk-r/ppa
 apt-get update
+apt-get upgrade -y
 
 eval $(dbus-launch --sh-syntax)
 export DBUS_SESSION_BUS_ADDRESS
 export DBUS_SESSION_BUS_PID
 export QT_X11_NO_MITSHM=1
-
 
